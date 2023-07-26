de3781e5a4549536bfbdd89c5ca98e436f4187fa
CHOI SI-HUN <sihun.choi@email.rhya-network.kro.kr>
2023-07-26 13:14:32 +0900

    dev commit

--- app/src/main/java/kro/kr/rhya_network/aicharacter_project/layout/activity/ACSplashActivity.java
+++ app/src/main/java/kro/kr/rhya_network/aicharacter_project/layout/activity/ACSplashActivity.java
@@ -1,8 +1,17 @@
 package kro.kr.rhya_network.aicharacter_project.layout.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
+
+import dev.shreyaspatil.MaterialDialog.BottomSheetMaterialDialog;
+import dev.shreyaspatil.MaterialDialog.interfaces.DialogInterface;
 import kro.kr.rhya_network.aicharacter_project.R;
 
 public class ACSplashActivity extends AppCompatActivity {
@@ -10,5 +19,55 @@ public class ACSplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ConstraintLayout loadingConstraintLayout = findViewById(R.id.loadingConstraintLayout);
+
+        // UI Animation
+        loadingConstraintLayout.animate()
+                .setStartDelay(500)
+                .alpha(1f)
+                .translationY(-50f)
+                .setDuration(1000)
+                .withEndAction(() -> {
+                })
+                .start();
+    }
+
+    protected void showSignInDialog() {
+        try {
+            BottomSheetMaterialDialog mBottomSheetDialog = new BottomSheetMaterialDialog.Builder(this)
+                    .setTitle("Login")
+                    .setMessage("구글 계정을 통해 로그인을 진행해주세요.")
+                    .setCancelable(false)
+                    .setAnimation(R.raw.lottie_anim_login)
+                    .setPositiveButton("Google 로그인", new BottomSheetMaterialDialog.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .setNegativeButton("앱 종료", new BottomSheetMaterialDialog.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+                            dialogInterface.dismiss();
+
+                            // 로그인 취소
+                            Toast.makeText(getApplicationContext(), "Login cancelled!", Toast.LENGTH_SHORT).show();
+                            // 앱 종료
+                            finishAffinity();
+                        }
+                    })
+                    .build();
+
+            LottieAnimationView animationView = mBottomSheetDialog.getAnimationView();
+            animationView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            animationView.setRepeatMode(LottieDrawable.RESTART);
+
+            // Show Dialog
+            mBottomSheetDialog.show();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/kro/kr/rhya_network/aicharacter_project/util/exception/ExceptionHandler.java
@@ -0,0 +1,4 @@
+package kro.kr.rhya_network.aicharacter_project.util.exception;
+
+public class ExceptionHandler {
+}
